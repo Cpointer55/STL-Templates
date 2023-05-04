4e6ab85139c385928e11aa8c5d0b4a2e8b214ab4
Moe <moe@moe>
2023-05-03 23:06:27 -0400

    Added Project Files

---
+++ STL Templates/Templates.cpp
@@ -0,0 +1,40 @@
+// Matthew Mondi
+// CIS 1202
+// 5/3/22
+
+#include<iostream>
+
+using namespace std;
+
+template <typename M>
+
+M half(M Number) {
+	 
+
+	M divided;
+
+	divided = Number / 2;
+
+	if (*typeid(Number).name() == 'I') {
+
+		if (divided * 2 != Number) {
+
+			divided++;
+		}
+	}
+
+	return divided;
+
+}
+
+int main() {
+
+	double a = 13.4;
+	float b = 6.0f;
+	int c = 5;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+}
